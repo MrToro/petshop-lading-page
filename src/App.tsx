a2861076d2fa9b61ce215e73d13564f0b5106ee8
Jose <ing.josetoro.2@gmail.com>
2024-01-12 16:50:49 -0500

    clear styled components sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import { Title, Wrapper } from "./App.Styles";
-
 const App: React.FC = () => {
-	return (
-		<Wrapper>
-			<Title>Hola mundo</Title>
-		</Wrapper>
-	);
+	return <></>;
 };
 
 export default App;
